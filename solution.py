3caa4b1d19527e3acdb799c9ab8c19a5d1197b40
voleka-ocrv <48094534+voleka-ocrv@users.noreply.github.com>
2023-04-12 12:01:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,12 +6,19 @@ from scipy.stats import norm
 
 chat_id = 440942058 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+//def solution(p: float, x: np.array) -> tuple:
+//    # Измените код этой функции
+//    # Это будет вашим решением
+//    # Не меняйте название функции и её аргументы
+//    alpha = 1 - p
+//    loc = x.mean() - 0.005
+//    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+//    return loc - scale * norm.ppf(1 - alpha / 2), \
+//           loc - scale * norm.ppf(alpha / 2)
+
+def solution(p, x):
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    t = x.max() - 0.005
+    n = len(x)
+    return 0.005 + t / ((1 - alpha / 2)**(1 / n)), \
+           0.005 + t / ((alpha / 2)**(1 / n))
